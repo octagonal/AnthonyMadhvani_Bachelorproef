dc4898f8922bca21ca3d66695c369ca35e966e57
Anthony Madhvani <anny.mdvn@gmail.com>
2016-02-21 22:29:29 +0100

    Clean up

--- paper/010_bootstrap.md
+++ paper/010_bootstrap.md
@@ -3,7 +3,15 @@
 
 ## Building Blocks
 
+Even though non-architectural design patterns are not the focus here, it is still nessecary to know a few basic patterns in order to be able to implement any architecture.
+They will be briefly explained here in a practical way.
+
 ### The Observer Pattern
+
+The observer pattern is at the basis of many common architectures. In fact, it is considered such a basic nessecity that many languages
+will ship with it (such as Java's Obverser class[@nystrom_design, chap. II.6]).
+
+### Mediator
 
 
 # Overview of several common patterns
--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
-pandoc <(cat paper/*.md) -o out/out.html --template template/template.html --css template/template.css --self-contained
-pandoc <(cat paper/*.md) -o out/outpandoc.pdf --toc -V papersize="a4paper" mainfont='Alegreya-Regular.otf'
 
+#Run a web server in the background
+#ruby -run -e httpd -- -p 5000 . &
+
+#Convert to HTML
+pandoc <(cat paper/*.md) -o out/out.html --template template/template.html --css template/template.css --self-contained --filter pandoc-citeproc --bibliography "citations.bib" --csl "apa-5th-edition"
+pandoc <(cat paper/*.md) -o out/outpandoc.pdf --toc -V papersize="a4paper" --filter pandoc-citeproc --bibliography "citations.bib"
+
+#Convert to PDF
 /opt/wkhtmltox/bin/wkhtmltopdf --footer-right "Page [page] of [toPage]" --header-right "[section]" toc --xsl-style-sheet "template/toc_stylesheet.xml" http://localhost:5000/out.html ./out/out.pdf
 
+#Merge paper and cover
 #pdfunite Cover.pdf out/outpandoc.pdf out/final.pdf
-
 pdfunite Cover.pdf out/out.pdf out/final.pdf
